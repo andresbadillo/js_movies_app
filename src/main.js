38a2555abed3bc29ad21a0cf7a00e90c8329b887
Andres Badillo <r.andres.badillo@gmail.com>
2023-01-26 15:33:47 -0500

    Modificacion en el diseño del footer 3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Holaaa');
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({data, movies});
+}
+
+getTrendingMoviesPreview();
